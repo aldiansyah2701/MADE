a8cdf446373931de458580ad661aa6853da37fe2
aldiansyah.ramadlan <aldiansyah.ramadlan@multipolar.com>
2019-09-27 23:08:48 +0700

    submision 4 finish

---
+++ app/src/main/java/com/example/aldiansyahramadlan/moviecatalogue_ui_ux/ListMovieFavorit.java
@@ -0,0 +1,13 @@
+package com.example.aldiansyahramadlan.moviecatalogue_ui_ux;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListMovieFavorit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_movie_favorit);
+    }
+}
---
+++ app/src/main/java/com/example/aldiansyahramadlan/moviecatalogue_ui_ux/database/DatabaseHelper.java
@@ -0,0 +1,4 @@
+package com.example.aldiansyahramadlan.moviecatalogue_ui_ux.database;
+
+public class DatabaseHelper {
+}
